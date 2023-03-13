9aeace5e4f1c743236a0314956c8046a24e5dbd7
shin-yuge <125860387+shin-yuge@users.noreply.github.com>
2023-03-13 22:11:45 +0900

    Add files via upload

---
+++ javascript/common.js
@@ -0,0 +1,15 @@
+function menu(){
+    var html = "";
+    html += '<ul class="menu">';
+    html += '<li class="mlist"><a class="m-item" href="index.html">TOP</a></li>';
+    html += '<li class="mlist"><a class="m-item" href="my-info.html">自己紹介</a></li>';
+    html += '<li class="mlist"><a class="m-item" href="my-talk.html">講演</a></li>';
+    html += '<li class="mlist"><a class="m-item" href="my-paper.html">論文・プレプリント</a></li>';
+    html += '<li class="mlist"><a class="m-item" href="my-graphics.html">グラフィックス</a></li>';
+    html += '<li class="mlist"><a class="m-item" href="my-class.html">担当授業</a></li>';
+    html += '<li class="mlist"><a class="m-item" href="my-schedule.html">スケジュール</a></li>';
+    html += '<li class="mlist"><a class="m-item" href="my-job.html">委員等</a></li>';
+    html += '<li class="mlist"><a class="m-item" href="my-link.html">リンク</a></li>';
+    html += '</ul>';
+    document.write(html);
+}
